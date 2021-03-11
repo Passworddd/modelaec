17b60867a16cb69f8831717e002c1a2818d24581
Nata Relva <natan.danilo@gmail.com>
2021-03-11 19:43:23 -0300

    PRIMEIRO COMMIT

---
+++ src/main/java/cursomc/cursomcprojeto/CursomcprojetoApplication.java
@@ -0,0 +1,13 @@
+package cursomc.cursomcprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcprojetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cursomc/cursomcprojeto/CursomcprojetoApplicationTests.java
@@ -0,0 +1,13 @@
+package cursomc.cursomcprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcprojetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
